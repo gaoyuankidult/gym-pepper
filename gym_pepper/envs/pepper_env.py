38814074253e94ea3359a83f27cbc27c6e88cd33
Yuan Gao <gaoyuankidult@gmail.com>
2018-02-01 15:33:02 +0100

    Updated the pepper enviroment.

--- gym_pepper/envs/pepper_env.py
+++ gym_pepper/envs/pepper_env.py
@@ -1,18 +1,286 @@
+import sys
+import numpy as np
+import cv2
+from naoqi import ALProxy
+
+import time
+import random 
+import thread
+import socket
+from multiprocessing import Value,Queue
+
 import gym
 from gym import error, spaces, utils
 from gym.utils import seeding
 
+
+
+            
 class PepperEnv(gym.Env):
     metadata = {'render.modes': ['human']}
 
     def __init__(self):
-        pass
+        # setup basic configerations
+        self.ip_addr = '130.238.17.115'
+        self.port_num = 9559
 
+        # create video interface
+        self.video_device = ALProxy('ALVideoDevice', self.ip_addr, self.port_num)
+
+
+        # subscribe top camera
+        # camera map
+        AL_kTopCamera = 0
+        AL_kQVGA = 1            # 320x240
+        AL_kQQVGA = 0            # 160x120
+        AL_kBGRColorSpace = 13
+        AL_kYUV422 = 9
+
+
+        try:
+            self.capture_device = self.video_device.unsubscribe()
+        except:
+            pass
+        finally:
+            self.capture_device = self.video_device.subscribeCamera(
+                "pepper_top_camera_%s"%time.time(), AL_kTopCamera, AL_kQQVGA, AL_kBGRColorSpace, 30)
+
+
+        self.screen_width = 160 # window configerations
+        self.screen_height = 120
+        self.image = np.zeros((self.screen_height, self.screen_width, 3), np.uint8)
+
+
+        # set observation and action spaces
+        self.observation_space = spaces.Box(low=0, high=255, shape=(self.screen_height, self.screen_width, 3))
+        self.action_space = spaces.Discrete(3) # Set with 8 elements {0, 1, 2, ..., 7}
+        
+        # remmeber the starting time
+        self.start_time = time.time()
+
+        # face detector configeration
+        self.memory = ALProxy("ALMemory",self.ip_addr,self.port_num)
+        self.session = self.memory.session()
+        
+        self.subscriber = self.session.service("ALMemory").subscriber("FaceDetected")
+        self.subscriber.signal.connect(self.on_human_tracked)
+        self.got_face = False
+        self.face_detection = ALProxy("ALFaceDetection",self.ip_addr,self.port_num)
+        self.face_detection.subscribe("HumanGreeter")
+
+    def __del__(self):
+        self.subscriber.unsubscribe("FaceDetected")
+        self.face_detection.unsubscribe("HumanGreeter")
+        self.capture_device = self.video_device.unsubscribe("pepper_top_camera")
+
+
+    def on_human_tracked(self,value):
+        if value == []:  # empty value when the face disappears
+            self.got_face = False
+        else:  # only speak the first time a face appears
+            self.got_face = True
+            
+    def touch_sensor(self):
+        if self.got_face:
+            return 4
+        else:
+            return -1
+
+    def wait(self):
+        time.sleep(0.2)
+        reward = 0.0001
+        return reward
+
+
+    def hello(self):
+        time.sleep(0.2)
+        if self.got_face:
+            reward = 0.5
+        else:
+            reward = -1
+        return reward
+
+        names = list()
+        times = list()
+        keys = list()
+
+        #names.append("LElbowRoll")
+        #times.append([1, 1.5, 2, 2.5])
+        #keys.append([-1.02102, -0.537561, -1.02102, -0.537561])
+
+        #names.append("LElbowYaw")
+        #times.append([1, 2.5])
+        #keys.append([-0.66497, -0.66497])
+
+        #names.append("LHand")
+        #times.append([2.5])
+        #keys.append([0.66])
+
+        #names.append("LShoulderPitch")
+        #times.append([1, 2.5])
+        #keys.append([-0.707571, -0.707571])
+
+        #names.append("LShoulderRoll")
+        #times.append([1, 2.5])
+        #keys.append([0.558505, 0.558505])
+
+        #names.append("LWristYaw")
+        #times.append([1, 2.5])
+        #keys.append([-0.0191986, -0.0191986])
+        #names2=["LElbowRoll","LElbowYaw","LHand","LShoulderPitch","LShoulderRoll","LWristYaw"]
+        #angles=[-0.479966,-0.561996,0.66,1.30202,0.195477, -0.637045]
+        #motion = ALProxy("ALMotion", self.ip_addr, self.port_num)
+        #motion.setExternalCollisionProtectionEnabled("Arms", False)
+        tts = ALProxy("ALTextToSpeech",self.ip_addr, self.port_num)
+        tts.setParameter("speed", 100)
+        tts.setLanguage("English")
+        #motion.angleInterpolation(names, keys, times, True)
+        tts.say("Hello")
+        #motion.setAngles(names2,angles,0.3)
+        if self.got_face:
+            reward = 0.5
+        else:
+            reward = -1
+        return reward
+
+    def shake_hand(self):
+        time.sleep(0.2)
+        r=self.touch_sensor()
+        if r>3:
+            time.sleep(0.2)
+        return r
+        
+        names = list()
+        times = list()
+        keys = list()
+        r=0
+        names.append("RHand")
+        times.append([2])
+        keys.append([0.98])
+
+
+
+        names.append("RShoulderPitch")
+        times.append([2])
+        keys.append([-0.2058])
+
+
+        names2=["RElbowRoll","RElbowYaw","RHand","RShoulderPitch","RShoulderRoll","RWristYaw"]
+        angles=[0.479966,0.561996,0.66,1.30202,-0.195477, 0.637045]
+
+        names3=["RHand"]
+        angles2=[0.4]
+
+        motion = ALProxy("ALMotion", self.ip_addr, self.port_num)
+        motion.setExternalCollisionProtectionEnabled("Arms", False)
+        tts = ALProxy("ALTextToSpeech",self.ip_addr, self.port_num)
+        tts.setParameter("speed", 60)
+        tts.setLanguage("English")
+        motion.setExternalCollisionProtectionEnabled("Arms", False)
+        motion.angleInterpolation(names, keys, times, True)
+        r=self.touch_sensor()
+        if r>3:
+                tts.say("Nice to meet you")
+                thread.start_new_thread(self.touch_sensor,())
+                motion.setAngles(names3,angles2,0.4)
+                time.sleep(2)
+        motion.setAngles(names2,angles,0.4)
+        return r
+
+    def perform_actions(self,action):
+
+#        print "The action is %d.\n1 and 2 are wait. 3 is hello and 4 is shake_hand."%action
+        
+        
+        if action==0: #wait
+            r=self.wait()
+
+        elif action==1: #hello
+            r=self.hello()
+
+        elif action==2: #shake
+            r=self.shake_hand()
+
+#        print "the reward number is %f"%r
+        return r
+    
     def _step(self, action):
-        pass
+        result = self.video_device.getImageRemote(self.capture_device);
+        if result == None:
+            print 'cannot capture.'
+        elif result[6] == None:
+            print 'no image data string.'
+        else:
+
+            # translate value to mat
+            values = map(ord, list(result[6]))
+            i = 0
+            for y in range(0, self.screen_height):
+                for x in range(0, self.screen_width):
+                    self.image.itemset((y, x, 0), values[i + 0])
+                    self.image.itemset((y, x, 1), values[i + 1])
+                    self.image.itemset((y, x, 2), values[i + 2])
+                    i += 3
+            
+        ob = self.image
+        reward = 0
+        if (time.time() - self.start_time) > 3:
+            episode_over = True
+            self.start_time = time.time()
+        else:
+            episode_over = False
+            
+        # step needs to be cleaned
+	reward = self.perform_actions(action)
+        #print "reward:", reward
+        #print 
+    
+        return ob, reward, episode_over, {}
+    
     
     def _reset(self):
-        pass
+        result = self.video_device.getImageRemote(self.capture_device);
+        if result == None:
+            print 'cannot capture.'
+        elif result[6] == None:
+            print 'no image data string.'
+        else:
+
+            # translate value to mat
+            values = map(ord, list(result[6]))
+            i = 0
+            for y in range(0, self.screen_height):
+                for x in range(0, self.screen_width):
+                    self.image.itemset((y, x, 0), values[i + 0])
+                    self.image.itemset((y, x, 1), values[i + 1])
+                    self.image.itemset((y, x, 2), values[i + 2])
+                    i += 3
+        
+        state = self.image
+        return state
     
     def _render(self, mode='human', close=False):
-        pass
+        result = self.video_device.getImageRemote(self.capture_device);
+        if result == None:
+            print 'cannot capture.'
+        elif result[6] == None:
+            print 'no image data string.'
+        else:
+
+            # translate value to mat
+            values = map(ord, list(result[6]))
+            i = 0
+            for y in range(0, self.screen_height):
+                for x in range(0, self.screen_width):
+                    self.image.itemset((y, x, 0), values[i + 0])
+                    self.image.itemset((y, x, 1), values[i + 1])
+                    self.image.itemset((y, x, 2), values[i + 2])
+                    i += 3
+    
+            # show image
+            cv2.imshow("pepper-top-camera-320x240", self.image)
+
+
+
+
+

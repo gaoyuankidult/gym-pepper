95461581ab62903fe4603512b56e433405b26840
Yuan Gao <gaoyuankidult@gmail.com>
2018-02-15 11:38:16 +0100

    Modified the pepper environment.

--- gym_pepper/envs/pepper_env.py
+++ gym_pepper/envs/pepper_env.py
@@ -1,7 +1,10 @@
 import sys
 import numpy as np
 import cv2
+
 from naoqi import ALProxy
+import qi
+import almath
 
 import time
 import random 
@@ -13,30 +16,33 @@ import gym
 from gym import error, spaces, utils
 from gym.utils import seeding
 
-
+import face_recognition
 
             
 class PepperEnv(gym.Env):
     metadata = {'render.modes': ['human']}
 
-    def __init__(self):
+    def __naoqi_config(self):
         # setup basic configerations
         self.ip_addr = '130.238.17.115'
         self.port_num = 9559
+        self.session = qi.Session()
+        try:
+            self.session.connect("tcp://" + self.ip_addr + ":" + str(self.port_num))
+        except RuntimeError:
+            print ("Can't connect to Naoqi at ip \"" + args.ip + "\" on port " + str(args.port) +".\n"
+                   "Please check your script arguments. Run with -h option for help.")
 
+    def __video_config(self):
         # create video interface
         self.video_device = ALProxy('ALVideoDevice', self.ip_addr, self.port_num)
 
-
         # subscribe top camera
-        # camera map
         AL_kTopCamera = 0
         AL_kQVGA = 1            # 320x240
         AL_kQQVGA = 0            # 160x120
         AL_kBGRColorSpace = 13
         AL_kYUV422 = 9
-
-
         try:
             self.capture_device = self.video_device.unsubscribe()
         except:
@@ -44,29 +50,68 @@ class PepperEnv(gym.Env):
         finally:
             self.capture_device = self.video_device.subscribeCamera(
                 "pepper_top_camera_%s"%time.time(), AL_kTopCamera, AL_kQQVGA, AL_kBGRColorSpace, 30)
-
-
         self.screen_width = 160 # window configerations
         self.screen_height = 120
         self.image = np.zeros((self.screen_height, self.screen_width, 3), np.uint8)
 
-
+    def __gym_config(self):
         # set observation and action spaces
         self.observation_space = spaces.Box(low=0, high=255, shape=(self.screen_height, self.screen_width, 3))
         self.action_space = spaces.Discrete(3) # Set with 8 elements {0, 1, 2, ..., 7}
+
+    def __face_detection_config(self):
+        # face recognizer configeration
+        alex_image = face_recognition.load_image_file("experiments/people/people-alex.jpg")
+        alex_face_encoding = face_recognition.face_encodings(alex_image)[0]
         
+        self.known_face_encodings = [
+            alex_face_encoding
+        ]
+        self.known_face_names = [
+            "Alex",
+        ]
+    def __body_state_config(self):
+        # disable autonomous life
+        self.life = ALProxy('ALAutonomousLife', self.ip_addr, self.port_num)
+        self.life.setAutonomousAbilityEnabled('All',False)
+        
+        self.awareness = self.session.service('ALBasicAwareness')
+        self.awareness.setEnabled(False)
+
+        self.leds = self.session.service('ALLeds')
+        self.leds.setIntensity('AllLeds', 1.0)
+
+        self.motion  = self.session.service("ALMotion")
+        self.motion.setStiffnesses("Head", 1.0)
+        names            = "HeadYaw"
+        angles           = 0.0*almath.TO_RAD
+        fractionMaxSpeed = 0.1
+        self.motion.setAngles(names,angles,fractionMaxSpeed)
+
+        names            = "HeadPitch"
+        angles           = 0.0*almath.TO_RAD
+        fractionMaxSpeed = 0.1
+        self.motion.setAngles(names,angles,fractionMaxSpeed)
+
+    def __init__(self):
+        self.__naoqi_config()
+        self.__video_config()
+        self.__gym_config()
+        self.__body_state_config()
+
         # remmeber the starting time
         self.start_time = time.time()
 
         # face detector configeration
         self.memory = ALProxy("ALMemory",self.ip_addr,self.port_num)
         self.session = self.memory.session()
-        
         self.subscriber = self.session.service("ALMemory").subscriber("FaceDetected")
         self.subscriber.signal.connect(self.on_human_tracked)
         self.got_face = False
         self.face_detection = ALProxy("ALFaceDetection",self.ip_addr,self.port_num)
         self.face_detection.subscribe("HumanGreeter")
+
+        self.__face_detection_config()
 
     def __del__(self):
         self.subscriber.unsubscribe("FaceDetected")
@@ -200,20 +245,19 @@ class PepperEnv(gym.Env):
 
         elif action==2: #shake
             r=self.shake_hand()
-
-#        print "the reward number is %f"%r
         return r
     
     def _step(self, action):
+
+        # Pre-process the information and store them in self.image
+
         result = self.video_device.getImageRemote(self.capture_device);
         if result == None:
             print 'cannot capture.'
         elif result[6] == None:
             print 'no image data string.'
         else:
-
-            # translate value to mat
-            values = map(ord, list(result[6]))
+            values = map(ord, list(result[6]))    # translate value to mat
             i = 0
             for y in range(0, self.screen_height):
                 for x in range(0, self.screen_width):
@@ -221,21 +265,41 @@ class PepperEnv(gym.Env):
                     self.image.itemset((y, x, 1), values[i + 1])
                     self.image.itemset((y, x, 2), values[i + 2])
                     i += 3
-            
+
+        # Process the image in order to get face information and also display it
+        
+        rgb_small_frame = self.image
+        face_locations = face_recognition.face_locations(rgb_small_frame)
+        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+        face_names = []
+        for face_encoding in face_encodings:
+            matches = face_recognition.compare_faces(self.known_face_encodings, face_encoding)
+            name = "Unknown"
+            if True in matches:
+                first_match_index = matches.index(True)
+                name = self.known_face_names[first_match_index]
+            face_names.append(name)
+        for (top, right, bottom, left), name in zip(face_locations, face_names):
+            cv2.rectangle(self.image, (left, top), (right, bottom), (0, 0, 255), 2)
+            cv2.rectangle(self.image, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(self.image, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+        cv2.imshow('video', self.image)
+        cv2.waitKey(1)
+        
+
+        
         ob = self.image
         reward = 0
-        if (time.time() - self.start_time) > 3:
+        if (time.time() - self.start_time) > 0.3:
             episode_over = True
             self.start_time = time.time()
         else:
             episode_over = False
-            
-        # step needs to be cleaned
 	reward = self.perform_actions(action)
-        #print "reward:", reward
-        #print 
-    
         return ob, reward, episode_over, {}
+
+    
     
     
     def _reset(self):
@@ -260,27 +324,12 @@ class PepperEnv(gym.Env):
         return state
     
     def _render(self, mode='human', close=False):
-        result = self.video_device.getImageRemote(self.capture_device);
-        if result == None:
-            print 'cannot capture.'
-        elif result[6] == None:
-            print 'no image data string.'
-        else:
-
-            # translate value to mat
-            values = map(ord, list(result[6]))
-            i = 0
-            for y in range(0, self.screen_height):
-                for x in range(0, self.screen_width):
-                    self.image.itemset((y, x, 0), values[i + 0])
-                    self.image.itemset((y, x, 1), values[i + 1])
-                    self.image.itemset((y, x, 2), values[i + 2])
-                    i += 3
-    
-            # show image
-            cv2.imshow("pepper-top-camera-320x240", self.image)
+        pass
+            
+ 
 
 
+                
 
 
 
